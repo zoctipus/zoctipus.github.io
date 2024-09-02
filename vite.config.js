4ab5bc728e6d54fe91d53a21a9b5565cad171bc3
Zhengyu Zhang <zzyoctopus@gmail.com>
2024-09-02 02:24:48 -0700

    fix url doesn't work issue

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,8 @@ export default {
     server:
     {
         host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
+        historyApiFallback: true
     },
     build:
     {
